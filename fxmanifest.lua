b412f78dcf67e0b1e8d5b904be1689f016c65a5d
CreatedByEric <48604050+createdbyeric@users.noreply.github.com>
2024-12-31 05:52:29 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,13 @@
 fx_version 'cerulean'
 game 'gta5'
 
-shared_script 'config.lua'
+author 'CreatedByEric'
+description 'CBE Engine Control Script'
+version '1.0.0'
+
+shared_scripts {
+    'config.lua'
+}
 
 client_scripts {
     'main/client.lua'

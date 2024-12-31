1f33d93368814c24998d678d0c066dac1ed93f2a
CreatedByEric <48604050+createdbyeric@users.noreply.github.com>
2024-12-30 20:14:02 -0500

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,11 @@
+Config = {
+    -- Engine Controls
+    ToggleEngineDriverOnly = true, -- Only allow the driver to toggle the engine
+    BlockControlsIfEngineOff = true, -- Block acceleration and brake if the engine is off
+    SaveEngineState = true, -- Save and restore engine state when entering/exiting vehicles
+    EngineAlwaysOn = true, -- Keep the engine running after exiting the vehicle
+
+    -- Exit Controls
+    AllowLongPressExit = true, -- Enable feature to leave the door open with a long press of the exit key
+    LongPressDuration = 250 -- Duration in milliseconds to trigger the long press action
+}

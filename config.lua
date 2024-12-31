9179066dfd9e5d8b137eb864e459c82ed2b4b417
CreatedByEric <48604050+createdbyeric@users.noreply.github.com>
2024-12-31 01:51:45 -0500

    Update config.lua
    v1.1.2

--- config.lua
+++ config.lua
@@ -1,11 +1,14 @@
 Config = {
     -- Engine Controls
-    ToggleEngineDriverOnly = true, -- Only allow the driver to toggle the engine
-    BlockControlsIfEngineOff = true, -- Block acceleration and brake if the engine is off
-    SaveEngineState = true, -- Save and restore engine state when entering/exiting vehicles
-    EngineAlwaysOn = true, -- Keep the engine running after exiting the vehicle
+    ToggleEngineDriverOnly = true,  -- Only allow the driver to toggle the engine
+    SaveEngineState = true,         -- Save and restore engine state when entering/exiting vehicles
+    EngineAlwaysOn = true,         -- Keep the engine running after exiting the vehicle
 
-    -- Exit Controls
-    AllowLongPressExit = true, -- Enable feature to leave the door open with a long press of the exit key
-    LongPressDuration = 250 -- Duration in milliseconds to trigger the long press action
+    -- Input Blocking
+    BlockControlsIfEngineOff = true,  -- Block acceleration and brake if the engine is off
+    DisableWheelTurningWhenEngineOff = true, -- Block wheel turning when the engine is off
+
+    -- Exit Features
+    AllowLongPressExit = true,      -- Enable feature to leave the door open with a long press of the exit key
+    LongPressDuration = 250        -- Duration (ms) to detect a long press for exiting the vehicle
 }
